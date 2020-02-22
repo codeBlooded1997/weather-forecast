c8314b4929442c6c7bcf55abfad89624c4df4dba
codeBlooded1997 <52186148+codeBlooded1997@users.noreply.github.com>
2020-02-22 09:21:19 -0500

    created record files

--- weatherforecast.py
+++ weatherforecast.py
@@ -1,7 +1,10 @@
+import datetime
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from twilio.rest import Client
 
+date = datetime.datetime.now()
 # grabbing page
 url = 'https://forecast.weather.gov/MapClick.php?lat=34.05349000000007&lon=-118.24531999999999'
 page = requests.get(url)
@@ -24,7 +27,25 @@ weather_stuff = pd.DataFrame(
         'temperature' : tempratures,
     })
 print(weather_stuff)
+print()
 
-# writng into a csv file
+
+# creating extention for csv file
+extention = str(date.strftime("%c"))
 # uncomment to create csv file
-#weather_stuff.to_csv('weather.csv')
+weather_stuff.to_csv(extention + '-weather.csv')
+
+print("CSV FILE CREATED")
+
+# Sending SMS
+account_sid = "AC81b4362f605c6398a48da412dcff6926"
+auth_token  = "84aa917ee50e1f59bf7c1d1264f7d1c3"
+client = Client(account_sid, auth_token)
+message = client.messages.create(
+   to="+14387004763",
+   from_="+16194326457",
+   body=str(weather_stuff)
+)
+
+print()
+print("A TEXT MASSAGE HAS BEEN SEND.")
